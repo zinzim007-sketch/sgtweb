3e9632bdf616898da402a1a25cc85d8aaf401fa9
zinzi <27996565@sun.ac.za>
2025-10-10 16:06:25 +0200

    Update backend API URL

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -21,7 +21,7 @@ export default function Contact() {
     setStatus("sending");
 
     try {
-      const res = await fetch("http://localhost:5000/api/contact", {
+      const res = await fetch("https://sgtweb-rpfo.onrender.com", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
