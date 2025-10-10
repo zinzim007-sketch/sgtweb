7e37a43b470b76e20444e923d3c97f396e2ccbe4
zinzi <zinzim007@gmail.com>
2025-10-10 22:49:41 +0200

    email should be fixed

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -21,7 +21,7 @@ export default function Contact() {
     setStatus("sending");
 
     try {
-      const res = await fetch("https://sgtweb-rpfo.onrender.com", {
+      const res = await fetch("https://sgtweb-rpfo.onrender.com/api/contact", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
