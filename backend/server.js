9e5505cb046445c9d3995b5f2f564b011b3dd1ad
Mdhluli, NZM, Miss [27996565@sun.ac.za] <zinzim007@gmail.com>
2026-08-19 01:04:24 +0200

    new email should be fixed now

--- backend/server.js
+++ backend/server.js
@@ -19,9 +19,9 @@ app.post("/api/contact", async (req, res) => {
 
   try {
     await resend.emails.send({
-      from: "Safeguard Website <onboarding@resend.dev>",
+      from: "SafeGuard Technologies <info@safeguardtechno.com>",
       to: process.env.RECEIVER_EMAIL, // this will send to info@safeguardtechno.com
-      subject: `📬 New message from ${name}`,
+      subject: `New message from ${name}`,
       text: `From: ${name}\nEmail: ${email}\n\n${message}`,
     });
 
